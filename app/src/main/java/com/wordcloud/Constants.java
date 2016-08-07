2a188c7bfd4d744e903e108df795cd165b3bc4c1
Himanshu <thehimanshugoel@gmail.com>
2016-08-07 11:45:58 +0530

    First Commit

---
+++ app/src/main/java/com/wordcloud/Constants.java
@@ -0,0 +1,25 @@
+package com.wordcloud;
+
+/**
+ * Created by Nidhi on 06-08-2016.
+ */
+public class Constants {
+    //fonts
+    public static final int FONT_TYPE_REGULAR = 1;
+    public static final int FONT_TYPE_THIN = 3;
+    public static final int FONT_TYPE_AMATIC = 4;
+    public static final int FONT_TYPE_ANKELCALLIG = 5;
+    public static final int FONT_TYPE_CHALKDUSTER = 6;
+    public static final int FONT_TYPE_CONJITRS = 7;
+    public static final int FONT_TYPE_EAGER = 8;
+    public static final int FONT_TYPE_EDOSZ = 9;
+    public static final int FONT_TYPE_INDIEFLOWER = 10;
+    public static final int FONT_TYPE_KRANKy = 11;
+    public static final int FONT_TYPE_LONDON = 12;
+    public static final int FONT_TYPE_LUCKIESTGUY = 13;
+    public static final int FONT_TYPE_PERMANENTMARKER = 14;
+    public static final int FONT_TYPE_RIBEYE = 15;
+    public static final int FONT_TYPE_ROCKSALT = 16;
+    public static final int FONT_TYPE_ZEYADA = 17;
+    public static final int FONT_TYPE_WOOD = 18;
+}
---
+++ app/src/main/java/com/wordcloud/CustomTypefaceSpan.java
@@ -0,0 +1,32 @@
+package com.wordcloud;
+
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.text.TextPaint;
+import android.text.style.MetricAffectingSpan;
+
+/**
+ * Created by Nidhi on 01-07-2016.
+ */
+public class CustomTypefaceSpan extends MetricAffectingSpan {
+    private final Typeface typeface;
+
+    public CustomTypefaceSpan(final Typeface typeface) {
+        this.typeface = typeface;
+    }
+
+    @Override
+    public void updateDrawState(final TextPaint drawState) {
+        apply(drawState);
+    }
+
+    @Override
+    public void updateMeasureState(final TextPaint paint) {
+        apply(paint);
+    }
+
+    private void apply(final Paint paint) {
+
+        paint.setTypeface(typeface);
+    }
+}
